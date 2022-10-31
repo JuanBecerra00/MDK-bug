0041e4ed0f4f669c2cdfb0d852284c14d3604e09
Juan Esteban Becerra Tapias <juan.becerra00@misena.edu.co>
2022-10-30 20:31:54 -0500

    V7

---
+++ script/homedots.js
@@ -0,0 +1,6 @@
+let dot0 = document.querySelector('.images-dot0');
+let dot1 = document.querySelector('.images-dot1');
+let dot2 = document.querySelector('.images-dot2');
+let dot3 = document.querySelector('.images-dot3');
+let dot4 = document.querySelector('.images-dot4');
+let dot5 = document.querySelector('.images-dot5');
--- script/script.js
+++ script/script.js
@@ -1,9 +1,26 @@
 let submenubutton = document.querySelector('.header-submenu-button');
+let submenubuttonlocker = document.querySelector('.header-submenu-button-locker');
 let submenu = document.querySelector('.header-submenu');
-submenubutton.onfocus = function () {
+let profile = document.querySelector('.submenu-profile');
+let settings = document.querySelector('.submenu-settings');
+let exit = document.querySelector('.submenu-exit');
+submenubutton.onclick = function () {
     submenubutton.classList.add('active')
     submenu.focus()
 }
+submenubuttonlocker.onclick = function () {
+    submenubutton.classList.add('active')
+    submenubuttonlocker.classList.remove('active')
+    submenubuttonlocker.classList.add('opacity-0')
+}
+submenu.onfocus = function () {
+    submenu.classList.toggle('active')
+    submenubuttonlocker.classList.add('active')
+    submenubuttonlocker.classList.remove('opacity-0')
+}
 submenu.onblur = function () {
     submenubutton.classList.remove('active')
+    submenu.classList.remove('active')
+    submenubuttonlocker.classList.remove('active')
+    submenubuttonlocker.classList.add('opacity-0')
 }
