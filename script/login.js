eea2c2408d967ed296237da195ab9d0187b43eb7
Juan Esteban Becerra Tapias <juan.becerra00@misena.edu.co>
2022-11-01 23:45:23 -0500

    Login modificado

--- script/login.js
+++ script/login.js
@@ -1,14 +1,24 @@
 let userid = document.querySelector('.form-id');
+let useridrecover = document.querySelector('.form-id-recover');
 let formpw = document.querySelector('.form-pw');
-let xid = document.querySelector('.error-x-id');
+let formnewpw = document.querySelector('.form-new-pw');
+let formnewpwconfirm = document.querySelector('.form-new-pw-confirm');
+let xid = document.querySelector('.error-x-id')
+let xidrecover = document.querySelector('.error-x-id-recover');
 let xpw = document.querySelector('.error-x-pw');
+let xnewpw = document.querySelector('.error-x-new-pw');
+let xnewpwconfirm = document.querySelector('.error-x-new-pw-confirm');
 let eyes = document.querySelector('.error-eyes');
 let loginbutton = document.querySelector('.form-button-login');
+let fomrbuttonrecovercancel = document.querySelector('.form-button-recover-cancel');
 let forgot = document.querySelector('.forgot');
-let logincontainer = document.querySelector('.login-container');
-let recovercancel = document.querySelector('.recover-button-cancel');
+let newpwsubmit = document.querySelector('.form-button-new-pw-submit');
 let errorid = document.querySelector('.error-message-id')
+let erroridrecover = document.querySelector('.error-message-id-recover')
 let errorpw = document.querySelector('.error-message-pw')
+let errornewpw = document.querySelector('.error-message-new-pw')
+let errornewpwconfirm = document.querySelector('.error-message-new-pw-confirm')
+let formcontainer = document.querySelector('.form-container');
 userid.onclick = function () {
     userid.onkeyup = function () {
         userid.classList.remove('shake')
@@ -19,6 +29,20 @@ userid.onclick = function () {
             userid.classList.add('shake')
             xid.classList.remove('opacity-0')
             errorid.classList.remove('opacity-0')
+            eyes.classList.add('error-visible')
+        }
+    }
+}
+useridrecover.onclick = function () {
+    useridrecover.onkeyup = function () {
+        useridrecover.classList.remove('shake')
+        xidrecover.classList.add('opacity-0')
+        erroridrecover.classList.add('opacity-0')
+        eyes.classList.remove('error-visible')
+        if (isNaN(useridrecover.value)) {
+            useridrecover.classList.add('shake')
+            xidrecover.classList.remove('opacity-0')
+            erroridrecover.classList.remove('opacity-0')
             eyes.classList.add('error-visible')
         }
     }
@@ -44,6 +68,51 @@ formpw.onclick = function () {
         }
     }
 }
+formnewpw.onclick = function () {
+    formnewpw.onkeyup = function () {
+        formnewpw.classList.remove('shake')
+        xnewpw.classList.add('opacity-0')
+        errornewpw.classList.add('opacity-0')
+        eyes.classList.remove('error-visible')
+    }
+    formnewpw.onblur = function () {
+        if (formnewpw.validity.valid==false) {
+            if (formnewpw.value==""){
+
+            }
+            else{
+            formnewpw.classList.add('shake')
+            xnewpw.classList.remove('opacity-0')
+            errornewpw.classList.remove('opacity-0')
+            eyes.classList.add('error-visible')
+            }
+        }
+    }
+}
+
+formnewpwconfirm.onclick = function () {
+    formnewpwconfirm.onkeyup = function () {
+        formnewpwconfirm.classList.remove('shake')
+        xnewpwconfirm.classList.add('opacity-0')
+        errornewpwconfirm.classList.add('opacity-0')
+        eyes.classList.remove('error-visible')
+    }
+    formnewpwconfirm.onblur = function () {
+        if (formnewpwconfirm.validity.valid==false) {
+            if (formnewpwconfirm.value==""){
+
+            }
+            else{
+            formnewpwconfirm.classList.add('shake')
+            xnewpwconfirm.classList.remove('opacity-0')
+            errornewpwconfirm.classList.remove('opacity-0')
+            eyes.classList.add('error-visible')
+            }
+        }
+    }
+}
+
+
 loginbutton.onclick = function() {
     if(userid.value==""){
         userid.classList.add('shake')
@@ -57,9 +126,14 @@ loginbutton.onclick = function() {
     }
 }
 forgot.onclick = function () {
-    logincontainer.classList.add('no')
+    formcontainer.classList.add('recover')
 }
-recovercancel.onclick = function () {
-    logincontainer.classList.remove('no')
+newpwsubmit.onclick = function () {
+    if (formnewpw.value!=formnewpwconfirm.value){
+        formnewpwconfirm.classList.add('shake')
+        xnewpwconfirm.classList.remove('opacity-0')
+        errornewpwconfirm.classList.remove('opacity-0')
+        eyes.classList.add('error-visible')
+        alert("Las contraseñas no son iguales");
+    }
 }
-
