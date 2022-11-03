b5ead2979103c0cc55875094d1e31976b1fceab4
SergioMoreno01 <samoreno491@misena.edu.co>
2022-11-03 12:01:35 -0500

    Login

--- script/login.js
+++ script/login.js
@@ -1,47 +1,21 @@
 let userid = document.querySelector('.form-id');
-let useridrecover = document.querySelector('.form-id-recover');
 let formpw = document.querySelector('.form-pw');
-let formnewpw = document.querySelector('.form-new-pw');
-let formnewpwconfirm = document.querySelector('.form-new-pw-confirm');
 let xid = document.querySelector('.error-x-id')
-let xidrecover = document.querySelector('.error-x-id-recover');
 let xpw = document.querySelector('.error-x-pw');
-let xnewpw = document.querySelector('.error-x-new-pw');
-let xnewpwconfirm = document.querySelector('.error-x-new-pw-confirm');
 let eyes = document.querySelector('.error-eyes');
 let loginbutton = document.querySelector('.form-button-login');
-let fomrbuttonrecovercancel = document.querySelector('.form-button-recover-cancel');
-let newpwsubmit = document.querySelector('.form-button-new-pw-submit');
 let errorid = document.querySelector('.error-message-id')
-let erroridrecover = document.querySelector('.error-message-id-recover')
 let errorpw = document.querySelector('.error-message-pw')
-let errornewpw = document.querySelector('.error-message-new-pw')
-let errornewpwconfirm = document.querySelector('.error-message-new-pw-confirm')
-let formcontainer = document.querySelector('.form-container');
 userid.onclick = function () {
     userid.onkeyup = function () {
         userid.classList.remove('shake')
         xid.classList.add('opacity-0')
         errorid.classList.add('opacity-0')
         eyes.classList.remove('error-visible')
-        if (isNaN(userid.value)) {
+        if (userid.validity.valid) {
             userid.classList.add('shake')
             xid.classList.remove('opacity-0')
             errorid.classList.remove('opacity-0')
-            eyes.classList.add('error-visible')
-        }
-    }
-}
-useridrecover.onclick = function () {
-    useridrecover.onkeyup = function () {
-        useridrecover.classList.remove('shake')
-        xidrecover.classList.add('opacity-0')
-        erroridrecover.classList.add('opacity-0')
-        eyes.classList.remove('error-visible')
-        if (isNaN(useridrecover.value)) {
-            useridrecover.classList.add('shake')
-            xidrecover.classList.remove('opacity-0')
-            erroridrecover.classList.remove('opacity-0')
             eyes.classList.add('error-visible')
         }
     }
@@ -67,49 +41,6 @@ formpw.onclick = function () {
         }
     }
 }
-formnewpw.onclick = function () {
-    formnewpw.onkeyup = function () {
-        formnewpw.classList.remove('shake')
-        xnewpw.classList.add('opacity-0')
-        errornewpw.classList.add('opacity-0')
-        eyes.classList.remove('error-visible')
-    }
-    formnewpw.onblur = function () {
-        if (formnewpw.validity.valid==false) {
-            if (formnewpw.value==""){
-
-            }
-            else{
-            formnewpw.classList.add('shake')
-            xnewpw.classList.remove('opacity-0')
-            errornewpw.classList.remove('opacity-0')
-            eyes.classList.add('error-visible')
-            }
-        }
-    }
-}
-
-formnewpwconfirm.onclick = function () {
-    formnewpwconfirm.onkeyup = function () {
-        formnewpwconfirm.classList.remove('shake')
-        xnewpwconfirm.classList.add('opacity-0')
-        errornewpwconfirm.classList.add('opacity-0')
-        eyes.classList.remove('error-visible')
-    }
-    formnewpwconfirm.onblur = function () {
-        if (formnewpwconfirm.validity.valid==false) {
-            if (formnewpwconfirm.value==""){
-
-            }
-            else{
-            formnewpwconfirm.classList.add('shake')
-            xnewpwconfirm.classList.remove('opacity-0')
-            errornewpwconfirm.classList.remove('opacity-0')
-            eyes.classList.add('error-visible')
-            }
-        }
-    }
-}
 
 
 loginbutton.onclick = function() {
@@ -122,14 +53,5 @@ loginbutton.onclick = function() {
         formpw.classList.add('shake')
         xpw.classList.remove('opacity-0')
         eyes.classList.add('error-visible')
-    }
-}
-newpwsubmit.onclick = function () {
-    if (formnewpw.value!=formnewpwconfirm.value){
-        formnewpwconfirm.classList.add('shake')
-        xnewpwconfirm.classList.remove('opacity-0')
-        errornewpwconfirm.classList.remove('opacity-0')
-        eyes.classList.add('error-visible')
-        alert("Las contraseñas no son iguales");
     }
 }
--- script/login2.js
+++ script/login2.js
@@ -14,15 +14,23 @@ let recovercontinue = document.querySelector('.recover-continue');
 let recoveronline = document.querySelector('.recover-button-online');
 let recoverlocal = document.querySelector('.recover-button-local');
 let recovermethodcancel = document.querySelector('.recover-method-cancel');
+let recoverquestioninput = document.querySelector('.recover-question-input');
 let recoverlocalcancel = document.querySelector('.recover-local-cancel');
 let recoverlocalcontinue = document.querySelector('.recover-local-continue');
+let recoveremailinput = document.querySelector('.recover-email-input');
 let recoveronlinecancel = document.querySelector('.recover-online-cancel');
 let recoveronlinecontinue = document.querySelector('.recover-online-continue');
 let recovercodecancel = document.querySelector('.recover-code-cancel');
 let recovercodecontinue = document.querySelector('.recover-code-continue');
+let recovernewpw = document.querySelector('.recover-new-pw');
+let recovernewpwconfirm = document.querySelector('.recover-new-pw-confirm');
 let recovernewpasswordcancel = document.querySelector('.recover-new-password-cancel');
 let recovernewpasswordcontinue = document.querySelector('.recover-new-password-continue');
 let resendemail = document.querySelector('.resend-email');
+let recoveridtype = document.querySelector('.recover-id-type');
+let recoveridinput = document.querySelector('.recover-id-input');
+let errorcontainer = document.querySelector('.error-container');
+let errortext = document.querySelector('.error-text');
 
 recoverlink.onclick = function () {
     cards.classList.remove('step-1')
@@ -45,16 +53,29 @@ recovercancel.onclick = function () {
 }
 
 recovercontinue.onclick = function () {
-    cards.classList.remove('step-1')
-    cards.classList.remove('step-2')
-    cards.classList.add('step-3')
-    cards.classList.remove('step-4')
-    cards.classList.remove('step-5')
-    cards.classList.remove('step-6')
-    cards.classList.remove('step-7')
+    if (recoveridtype.validity.valid == true & recoveridinput.validity.valid == true) {
+        cards.classList.remove('step-1')
+        cards.classList.remove('step-2')
+        cards.classList.add('step-3')
+        cards.classList.remove('step-4')
+        cards.classList.remove('step-5')
+        cards.classList.remove('step-6')
+        cards.classList.remove('step-7')
+    }else{
+        errortext.innerText = "Datos invalidos"
+        }
 }
 
 recoveronline.onclick = function () {
+    if (recoveridtype.validity.valid) {
+        cards.classList.remove('step-1')
+        cards.classList.remove('step-2')
+        cards.classList.add('step-3')
+        cards.classList.remove('step-4')
+        cards.classList.remove('step-5')
+        cards.classList.remove('step-6')
+        cards.classList.remove('step-7')
+    }
     cards.classList.remove('step-1')
     cards.classList.remove('step-2')
     cards.classList.remove('step-3')
@@ -94,13 +115,15 @@ recoverlocalcancel.onclick = function () {
 }
 
 recoverlocalcontinue.onclick = function () {
-    cards.classList.remove('step-1')
-    cards.classList.remove('step-2')
-    cards.classList.remove('step-3')
-    cards.classList.remove('step-4')
-    cards.classList.remove('step-5')
-    cards.classList.remove('step-6')
-    cards.classList.add('step-7')
+    if (recoverquestioninput.validity.valid) {
+        cards.classList.remove('step-1')
+        cards.classList.remove('step-2')
+        cards.classList.remove('step-3')
+        cards.classList.remove('step-4')
+        cards.classList.remove('step-5')
+        cards.classList.remove('step-6')
+        cards.classList.add('step-7')
+    }
 }
 recoveronlinecancel.onclick = function () {
     cards.classList.remove('step-1')
@@ -113,39 +136,39 @@ recoveronlinecancel.onclick = function () {
 }
 
 recoveronlinecontinue.onclick = function () {
-    email = document.querySelector('.form-email-recover');
-    if (email.value == 7) {
+    if (recoveremailinput.value == 'test@gmail.com') {
         console.log("test")
+        console.log(randomnumber)
         cards.classList.remove('step-1'),
-        cards.classList.remove('step-2'),
-        cards.classList.remove('step-3'),
-        cards.classList.remove('step-4'),
-        cards.classList.remove('step-5'),
-        cards.classList.add('step-6'),
-        cards.classList.remove('step-7')
-    }else{
+            cards.classList.remove('step-2'),
+            cards.classList.remove('step-3'),
+            cards.classList.remove('step-4'),
+            cards.classList.remove('step-5'),
+            cards.classList.add('step-6'),
+            cards.classList.remove('step-7')
+    } else if (recoveremailinput.validity.valid) {
         document.querySelector('.verification').value = randomnumber;
 
-    const buttonsendemail = document.querySelector('.form-button-send-email');
+        const buttonsendemail = document.querySelector('.form-button-send-email');
 
 
-    Email.send({
-        SecureToken: "b91fe74e-b4b0-4f88-b2e8-64667525c353",
-        To: email.value,
-        From: "megadatakit@gmail.com",
-        Subject: "Codigo de recuperacion cuenta MDK",
-        Body: randomnumber
-    }).then(
-        message => alert("Codigo enviado"),
-        cards.classList.remove('step-1'),
-        cards.classList.remove('step-2'),
-        cards.classList.remove('step-3'),
-        cards.classList.remove('step-4'),
-        cards.classList.remove('step-5'),
-        cards.classList.add('step-6'),
-        cards.classList.remove('step-7'),
-        console.log(randomnumber)
-    );
+        Email.send({
+            SecureToken: "b91fe74e-b4b0-4f88-b2e8-64667525c353",
+            To: recoveremailinput.value,
+            From: "megadatakit@gmail.com",
+            Subject: "Codigo de recuperacion cuenta MDK",
+            Body: randomnumber
+        }).then(
+            message => alert("Codigo enviado"),
+            cards.classList.remove('step-1'),
+            cards.classList.remove('step-2'),
+            cards.classList.remove('step-3'),
+            cards.classList.remove('step-4'),
+            cards.classList.remove('step-5'),
+            cards.classList.add('step-6'),
+            cards.classList.remove('step-7'),
+            console.log(randomnumber)
+        );
     }
 }
 
@@ -155,12 +178,11 @@ resendemail.onclick = function () {
     document.querySelector('.verification').value = randomnumber;
 
     const buttonsendemail = document.querySelector('.form-button-send-email');
-    email = document.querySelector('.form-email-recover');
 
 
     Email.send({
         SecureToken: "b91fe74e-b4b0-4f88-b2e8-64667525c353",
-        To: email.value,
+        To: recoveremailinput.value,
         From: "megadatakit@gmail.com",
         Subject: "reenvio de codigo de recuperacion cuenta MDK",
         Body: randomnumber
@@ -207,18 +229,20 @@ recovernewpasswordcancel.onclick = function () {
 }
 
 recovernewpasswordcontinue.onclick = function () {
-    if (checkrn.value == randomnumber) {
-        cards.classList.add('step-1')
-        cards.classList.remove('step-2')
-        cards.classList.remove('step-3')
-        cards.classList.remove('step-4')
-        cards.classList.remove('step-5')
-        cards.classList.remove('step-6')
-        cards.classList.remove('step-7')
-    } else {
-        console.log("no")
-        console.log(checkrn.value)
-        console.log(randomnumber)
+    if (recovernewpw.validity.valid & recovernewpwconfirm.validity.valid) {
+        if (recovernewpw.value == recovernewpwconfirm.value) {
+            cards.classList.add('step-1')
+            cards.classList.remove('step-2')
+            cards.classList.remove('step-3')
+            cards.classList.remove('step-4')
+            cards.classList.remove('step-5')
+            cards.classList.remove('step-6')
+            cards.classList.remove('step-7')
+        } else {
+            console.log("contraseñas diferentes")
+        }
+    }else {
+        console.log('Datos invalidos')
     }
 
 }
